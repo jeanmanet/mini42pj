31f69ce64f1aff54dded6e39fbaf3f8fbb209466
ORY <ory@MacBook-Air-de-Ory.local>
2023-05-12 01:46:27 +0200

    ft_echo -n -nnnnnnn fix

--- srcs/exec/builtins/builtins_echo.c
+++ srcs/exec/builtins/builtins_echo.c
@@ -3,29 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   builtins_echo.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmanet <jmanet@student.42nice.fr>          +#+  +:+       +#+        */
+/*   By: ory <ory@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 20:09:11 by jmanet            #+#    #+#             */
-/*   Updated: 2023/05/10 12:16:29 by jmanet           ###   ########.fr       */
+/*   Updated: 2023/05/12 01:43:36 by ory              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
+int	is_param_n(char *str)
+{
+	int	i;
+
+	i = 1;
+	if (str[0] == '-')
+	{
+		while (str[i])
+		{
+			if (str[i] != 'n')
+				return (0);
+			i++;
+		}
+		return (1);
+	}
+	return (0);
+}
+
 int	ft_echo(t_com *command)
 {
 	int	i;
 
-	i = 0;
+	i = 1;
 	if (!command->args[1])
 	{
 		printf("\n");
 		return (0);
 	}
-	if (!ft_strncmp(command->args[1], "-n", 3))
-		i = 2;
-	else
-		i = 1;
+	while (command->args[i])
+	{
+		if (!is_param_n(command->args[i]))
+			break ;
+		i++;
+	}
 	while (command->args[i])
 	{
 		ft_putstr_fd(command->args[i], 1);
@@ -33,7 +53,7 @@ int	ft_echo(t_com *command)
 		if (command->args[i])
 			write(1, " ", 1);
 	}
-	if (ft_strncmp(command->args[1], "-n", 3))
+	if (!is_param_n(command->args[1]))
 		write(1, "\n", 1);
 	return (0);
 }
