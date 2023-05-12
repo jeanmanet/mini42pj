dd2fe41561bc1c6a57db0d7e295a4163e649db50
ORY <ory@MacBook-Air-de-Ory.local>
2023-05-12 07:44:07 +0200

    add alphabetic print order in export

--- srcs/exec/builtins/ft_export_utils.c
+++ srcs/exec/builtins/ft_export_utils.c
@@ -3,23 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmanet <jmanet@student.42nice.fr>          +#+  +:+       +#+        */
+/*   By: ory <ory@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 13:46:07 by ory               #+#    #+#             */
-/*   Updated: 2023/05/07 13:26:57 by jmanet           ###   ########.fr       */
+/*   Updated: 2023/05/12 07:41:52 by ory              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-void	ft_env_export(t_data *data)
+void	ft_free_envp(char **tab)
 {
 	int	i;
 
 	i = 0;
-	while (data->envp[i])
+	while (tab[i])
 	{
-		printf("declare -x %s\n", data->envp[i]);
+		free(tab[i]);
 		i++;
 	}
+	free(tab);
+}
+
+void	sort_env(char **envp)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 0;
+	while (envp[i])
+	{
+		j = i + 1;
+		while (envp[j])
+		{
+			if (ft_strcmp(envp[i], envp[j]) > 0)
+			{
+				temp = envp[i];
+				envp[i] = envp[j];
+				envp[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ft_env_export(t_data *data)
+{
+	int		i;
+	char	**envp_copy;
+
+	i = 0;
+	envp_copy = ft_import_envp(data->envp);
+	sort_env(envp_copy);
+	while (envp_copy[i])
+	{
+		printf("declare -x %s\n", envp_copy[i]);
+		i++;
+	}
+	ft_free_envp(envp_copy);
 }
